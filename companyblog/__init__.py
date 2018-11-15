7d810df5b29a464d1c9937883a444cbfd64c1139
Oscar <oscarwasil@gmail.com>
2018-11-15 15:23:53 -0600

    Added user forms

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -1,9 +1,31 @@
 # companyblog/__init__.py
-
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 
+### Databse Setup ###
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+####################
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'user.login'
+
+
+
+
+####################
 from companyblog.core.views import core
 from companyblog.error_pages.handlers import error_pages
 
