db8d084c7ea05f3ce9c7e62519ec565e594160ce
Oscar <oscarwasil@gmail.com>
2018-11-17 16:42:22 -0600

    Updated views and added 2 html files

--- companyblog/users/views.py
+++ companyblog/users/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,url_for,flash,redirect,request,Blueprint
-from flask_login import login_user,current_user,logout_user,login_required
+from flask_login import login_user, current_user, logout_user, login_required
 from companyblog import db
 from companyblog.models import User, BlogPost
 from companyblog.users.forms import RegistrationForm,LoginForm,UpdateUserForm
@@ -7,70 +7,78 @@ from companyblog.users.picture_handler import add_profile_pic
 
 users = Blueprint('users',__name__)
 
+# register
 @users.route('/register',methods=['GET','POST'])
 def register():
-  form = RegistrationForm()
+    form = RegistrationForm()
 
-  if form.validate_on_submit():
-    user = User(email=form.email.data,username=form.username.data,password=form.password.data)
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
 
-    db.session.add(user)
-    db.session.commit()
-    # flash('Thanks for registration')
-    return redirect(url_for('users.login'))
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for registration!')
+        return redirect(url_for('users.login'))
 
-  return render_template('register.html',form=form)
+    return render_template('register.html',form=form)
 
+
+
+# login
 @users.route('/login',methods=['GET','POST'])
 def login():
 
-  form = LoginForm()
-  if form.validate_on_submit():
+    form = LoginForm()
+    if form.validate_on_submit():
 
-    user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
 
-    if user.check_password(form.password.data) and user is not None:
+        if user.check_password(form.password.data) and user is not None:
 
-      login_user(user)
-      flash('Log in Success')
+            login_user(user)
+            flash('Log in Success!')
 
-      next = request.args.get('next')
-      if next ==None or not next[0]=='/':
-        next = url_for('core.index')
+            next = request.args.get('next')
 
-      return redirect(next)
+            if next ==None or not next[0]=='/':
+                next = url_for('core.index')
 
-return render_template('login.html',form=form)
+            return redirect(next)
+
+    return render_template('login.html',form=form)
 
 
 
 @users.route("/logout")
 def logout():
-  logout_user()
-  return redirect(url_for("core.index"))
+    logout_user()
+    return redirect(url_for("core.index"))
 
-@user.route('/account',methods=['GET','POST'])
+
+
+@users.route('/account',methods=['GET','POST'])
 @login_required
 def account():
 
-  form = UpdateUserForm()
-  if form.validate_on_submit():
+    form = UpdateUserForm()
+    if form.validate_on_submit():
 
-    if form.picture.data:
-      username = current_user.username
-      pic = add_profile_pic(form.picture.data,username)
-      current_user.profile_image = pic
+        if form.picture.data:
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data,username)
+            current_user.profile_image = pic
 
-    current_user.username = form.username.data
-    current_user.email = form.email.datadb.session.commit()
-    return redirect(url_for('users.account'))
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('User Account Updated!')
+        return redirect(url_for('users.account'))
 
-  elif request.method == "GET":
-    form.username.data = current_user.username
-    form.email.data = current_user.email
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
 
-  profile_image = url_for('static',filename='profile_pics/'+current_user.profile_image)
-
-
-
-
+    profile_image = url_for('static',filename='profile_pics/'+current_user.profile_image)
+    return render_template('account.html',profile_image=profile_image,form=form)
