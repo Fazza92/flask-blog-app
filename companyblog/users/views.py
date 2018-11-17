d7e102dd20302c15a2c04d570e25939c9bcc507e
Oscar <oscarwasil@gmail.com>
2018-11-17 12:47:35 -0600

    Updated user views

--- companyblog/users/views.py
+++ companyblog/users/views.py
@@ -48,3 +48,29 @@ return render_template('login.html',form=form)
 def logout():
   logout_user()
   return redirect(url_for("core.index"))
+
+@user.route('/account',methods=['GET','POST'])
+@login_required
+def account():
+
+  form = UpdateUserForm()
+  if form.validate_on_submit():
+
+    if form.picture.data:
+      username = current_user.username
+      pic = add_profile_pic(form.picture.data,username)
+      current_user.profile_image = pic
+
+    current_user.username = form.username.data
+    current_user.email = form.email.datadb.session.commit()
+    return redirect(url_for('users.account'))
+
+  elif request.method == "GET":
+    form.username.data = current_user.username
+    form.email.data = current_user.email
+
+  profile_image = url_for('static',filename='profile_pics/'+current_user.profile_image)
+
+
+
+
